f7af6b25fe68c5b7fcccb1134db0dbb0e59907ee
hhy <450425947@qq.com>
2015-08-30 13:54:55 +0800

    new file:   close.js 	new file:   light.js 	new file:   on.js 	new file:   test_close.js 	new file:   test_on.js

---
+++ test_on.js
@@ -0,0 +1,19 @@
+var hue = require('./lib/hue');
+
+//hue.discover(function (error, host) {
+//  if (error) {
+//    console.error(error);
+//    return;
+//  }
+var host = "192.168.0.101"
+  
+console.log("Discovered HUE at %s", host);
+
+hue.load(host, "27df1d5a28d980171859b0c669edb33"); // need to register this first (not currently supported)
+
+hue.lights(function(lights){
+      for(i in lights)
+          if(lights.hasOwnProperty(i))
+              hue.change(lights[i].set({"on": false , transitiontime=20}));
+});
+
